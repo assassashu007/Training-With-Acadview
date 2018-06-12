35ea1e6248d3431298b4696c50f0658942d3b80b
Ashutosh <assassashu007@gmail.com>
2018-06-13 01:05:51 +0530

    functions in python

---
+++ assignment_6.py
@@ -0,0 +1,76 @@
+print("Q.1- Create a function to calculate the area of a circle by taking radius from user.\n")
+
+
+def cal_area(r):
+    print(3.14 * r * r)
+
+cal_area(float(input("Enter radius of circle : ")))
+
+
+
+
+
+print('\n\nQ.2- Write a function “perfect()” that determines perfect numbers between 1 and 1000.\n')
+
+
+def perfect(n):
+    sum = 0
+    for x in range(1, n):
+        if n % x == 0:
+            sum += x
+    if sum == n:
+        print(str(n) + " is a perfect number.")
+
+for i in range(1,1001):
+    perfect(i)
+
+
+
+
+
+print("\n\nQ.4- Write a function to calculate power of a number raised to other ( a^b ) using recursion.\n")
+
+
+def power(base,exp):
+    if exp == 1:
+        return(base)
+    if exp != 1:
+        return(base*power(base,exp-1))
+
+
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential value: "))
+print("Result: " + str(power(base,exp)))
+
+
+
+
+
+print("\n\nQ.5- Write a function to find factorial of a number but also store the factorials calculated in a dictionary.\n")
+
+dic = []
+index = []
+
+
+def fact(a):
+    dic.append(a)
+    if a == 1:
+        dictionary = dict(zip(index, dic))
+        print(dictionary)
+        return(a)
+    if a != 1:
+        return(a*fact(a-1))
+
+
+
+x = int(input("Enter a number to get its factorial : "))
+d =x
+
+for d in range(0, d):
+    index.append("value " + str(d+1))
+
+
+print("\nFactorial is : " + str(fact(x)))
+
+
+
